819aa1cf69503a457b455bc935b413d5c2ee8d96
angelDi0 <angel.diosdado1239@gmail.com>
2026-05-17 17:37:55 -0600

    Merge con la rama principal

--- shared/src/commonMain/kotlin/com/example/sicedroidmult/DB/Entidad/Entidades.kt
+++ shared/src/commonMain/kotlin/com/example/sicedroidmult/DB/Entidad/Entidades.kt
@@ -34,3 +34,30 @@ data class KardexItem(
     val Calif: String = "",
     val Acred: String = ""
 )
+
+@Serializable
+data class CalificacionesUnidadItem(
+    val Materia: String? = null,
+    val Grupo: String? = null,
+    val C1: String? = null,
+    val C2: String? = null,
+    val C3: String? = null,
+    val C4: String? = null,
+    val C5: String? = null,
+    val C6: String? = null,
+    val C7: String? = null,
+    val C8: String? = null,
+    val C9: String? = null,
+    val C10: String? = null,
+    val C11: String? = null,
+    val C12: String? = null,
+    val C13: String? = null
+)
+
+@Serializable
+data class CalificacionFinalItem(
+    val materia: String = "",
+    val grupo: String = "",
+    val calif: String = "",
+    val acred: String = ""
+)
